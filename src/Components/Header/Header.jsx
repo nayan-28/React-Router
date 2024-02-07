6ed086df7fde3b6be5275ae1190748db18d2ecf6
nayan-28 <nayanmalakar720@gmail.com>
2024-02-07 20:54:05 +0600

    Active route

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <nav>
         <span>My Website</span>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="contact">Contact</Link>
-        <Link to="/users">Users</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/users">Users</NavLink>
       </nav>
     </div>
   );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import "../Header/Header.css";
 import Footer from "../Footer/Footer";
 const Home = () => {
+  const navigation = useNavigation();
   return (
     <div>
       <Header></Header>
-      <Outlet></Outlet>
+      {navigation.state === "Loading" ? <p>Loading.....</p> : <Outlet></Outlet>}
+
       <Footer></Footer>
     </div>
   );
